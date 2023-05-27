1d00dcce7da94c73c9665bcfd8c58dd73d1160ce
Razvan <popescu.razvanmihai94@gmail.com>
2023-05-27 19:20:46 +0300

    <inputbar+league api updates>

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,48 @@ import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import apiKey from "../data/config";
 
 const Home = ({ matchId, setMatchId, setTeams }) => {
-  const [data, setData] = useState(null);
+  const [league, setLeague] = useState(null);
   const [matches, setMatches] = useState(null);
+  const [input, setInput] = useState("");
+  const [countries, setCountries] = useState([]);
 
+
+
+function handleInputChange(event) {
+  setInput(event.target.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split("T")[0]; // Format the current date as "YYYY-MM-DD"
+
+  axios({
+      method: "GET",
+      url: "https://api-football-v1.p.rapidapi.com/v3/fixtures",
+      params: { date: formattedDate },
+      headers: {
+        "X-RapidAPI-Key": apiKey,
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+      },
+    })
+      .then((response) => {
+        console.log(response.data.response, "response");
+
+        const matches = response.data.response.filter((fixture) => {
+          console.log(fixture.league.country, "fixture");
+          return fixture.league.country === input;
+        });
+        console.log(matches, "matches");
+        setMatches(matches);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      setInput("");
+     }
+    
  
 
   function getData(id, teams) {
@@ -33,10 +72,14 @@ const Home = ({ matchId, setMatchId, setTeams }) => {
   }
 
   function Json() {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString().split("T")[0]; // Format the current date as "YYYY-MM-DD"
+    console.log(formattedDate, "formattedDate");
+    
     axios({
       method: "GET",
       url: "https://api-football-v1.p.rapidapi.com/v3/fixtures",
-      params: { date: "2023-05-23" },
+      params: { date: formattedDate },
       headers: {
         "X-RapidAPI-Key": apiKey,
         "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
@@ -44,13 +87,16 @@ const Home = ({ matchId, setMatchId, setTeams }) => {
     })
       .then((response) => {
         console.log(response.data.response, "response");
-
-        const matches = response.data.response.filter((fixture) => {
+  
+        const matches = response.data.response.map((fixture) => {
           console.log(fixture.league.country, "fixture");
-          return fixture.league.country === "Colombia";
+          return fixture.league.country;
         });
-        console.log(matches, "matches");
-        setMatches(matches);
+  
+        const uniqueCountries = Array.from(new Set(matches)); // Remove duplicates using Set
+  
+        console.log(uniqueCountries, "uniqueCountries");
+        setCountries(uniqueCountries);
       })
       .catch((error) => {
         console.log(error);
@@ -62,11 +108,28 @@ const Home = ({ matchId, setMatchId, setTeams }) => {
       <div>
         <h1>Home</h1>
       </div>
-      <h2>Live Leagues</h2>
-
       <button onClick={Json} type="button" className="btn btn-primary">
-        Get Brazil
+        Get Available Leagues
       </button>
+      <h2>Available Leagues</h2>
+      <div className="w-100 d-flex flex-wrap">
+      {countries && countries.map((item) => {
+        return <p className="league_button">{item}</p>
+      })
+      }
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          League
+        </label>
+          <input type="text" name="input" value={input} onChange={handleInputChange}/>
+       
+        <input type="submit" value="Submit" />
+
+      </form>
+
+      
       <table className="table-success table table-striped table-hover custom-table">
         <thead>
           <tr>
