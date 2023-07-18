21c3f8358cabc0b3546cc9e4210027b8c762866e
razvan712 <111775069+razvan712@users.noreply.github.com>
2023-07-18 15:41:33 +0300

    login with google

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,24 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { Button, Form, Alert } from "react-bootstrap";
+import jwt_decode from "jwt-decode"; // Import JWT decoding library
 import "./Login.scss";
+import google from '../assets/google.png'
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  
-  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  const { login } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (username === "admin" && password === "admin") {
-      setIsLoggedIn(true);
+      // Simulating successful authentication by generating a JWT token
+      const token = "your_generated_jwt_token_here";
+      login(token);
       console.log("Logged in");
     } else {
       setError("Wrong username or password");
@@ -26,6 +30,33 @@ const Login = () => {
       <h1>RazBet</h1>
 
       <Form onSubmit={handleSubmit}>
+      <Button
+      style={{
+        backgroundColor: "#ffffff",
+        border: "1px solid #dddddd",
+        borderRadius: "4px",
+        padding: "10px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        cursor: "pointer",
+        width: "200px",
+        height: "40px",
+        margin: "10px",
+        
+      }}
+    >
+      <img
+        src={google} // Replace with your Google icon image source
+        alt="Google Icon"
+        style={{
+          width: "20px",
+          height: "20px",
+          marginRight: "10px",
+        }}
+      />
+     <span>Sign in with Google</span> 
+    </Button>
         <Form.Group controlId="username">
           <Form.Label>Username</Form.Label>
           <Form.Control
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,10 +3,22 @@ import React, { createContext, useState } from "react";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
+
+  const login = (jwtToken) => {
+    setToken(jwtToken);
+  };
+
+  const logout = () => {
+    setToken(null);
+  };
+
+  const isAuthenticated = () => {
+    return token !== null;
+  };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <AuthContext.Provider value={{ token, login, logout, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
