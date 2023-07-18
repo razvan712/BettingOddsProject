40b45a1a76c0ecae4c0c9069b3a92907d513e914
razvan712 <111775069+razvan712@users.noreply.github.com>
2023-07-18 10:32:33 +0300

    about page restructure

--- src/components/About.js
+++ src/components/About.js
@@ -3,21 +3,26 @@ import './About.scss';
 
 const About = () => {
   return (
-    <div className="about-page bg-success-subtle">
-      <h2>About</h2>
+    <div className="about-page ">
+      <h1>About</h1>
       <ul>
         <li>
-         <p> Welcome to our betting odds app! We provide the latest and most accurate odds for various matches across different sports. Our goal is to help you make informed betting decisions and increase your chances of winning.</p>
+          <h2>Company</h2>
+         <p> Welcome to <span>RazBet</span>, the premier destination for football betting odds and analysis. As a trusted and reliable platform, we are dedicated to providing you with the most accurate and up-to-date information to enhance your sports betting experience. Our team of passionate football enthusiasts works tirelessly to bring you the latest odds, expert predictions, and comprehensive statistics, ensuring that you stay one step ahead in the game.
+
+At <span>RazBet</span>, we pride ourselves on our commitment to excellence. We have built a reputation for delivering a user-friendly interface, cutting-edge technology, and a secure platform that prioritizes your privacy and protection. Whether you are a seasoned bettor or just starting out, we strive to create an environment that empowers you to make informed decisions and maximize your chances of success.
+
+Join our community today and gain access to a wealth of football betting resources, including real-time odds, in-depth match analysis, betting strategies, and exclusive tips from industry experts. We believe that everyone deserves a fair shot at winning, and our mission is to be your trusted companion on this thrilling journey.</p>
         </li>
         <li>
-         <p>Whether you're a seasoned bettor or just starting out, our app offers a user-friendly interface and a wide range of matches to choose from. You can explore different leagues and events and access detailed information about each match's odds.</p> 
+          <h2>Vision</h2>
+         <p>At <span>RazBet</span>, our vision is to revolutionize the way football betting is approached and experienced. We aim to be the go-to destination for sports enthusiasts seeking accurate and comprehensive information to inform their betting decisions. By combining cutting-edge technology, data-driven insights, and a passion for the game, we strive to empower our users with the tools they need to excel in the world of football betting.
+
+We envision a future where every bettor, from novices to professionals, can access reliable and transparent odds, analyze matches from various perspectives, and make well-informed choices. We are committed to delivering a seamless and intuitive user experience, ensuring that our users can easily navigate our platform, find the information they need, and take advantage of the latest trends and opportunities in the football betting landscape.
+
+At <span>RazBet</span>, we value integrity, trust, and responsible gambling practices. Our vision extends beyond mere profit, as we believe in promoting a safe and enjoyable betting environment. We actively encourage responsible gambling habits and advocate for ethical standards within the industry. Together, let's embrace the excitement of football betting while maintaining a mindful approach that prioritizes entertainment, strategic thinking, and responsible decision-making.</p> 
         </li>
-        <li>
-          <p>We strive to continuously update our odds data to ensure you have access to the most up-to-date information. Additionally, we provide helpful resources, tips, and strategies to enhance your understanding of betting odds and improve your overall betting experience.</p>
-        </li>
-        <li>
-          <p>If you have any questions, suggestions, or feedback, please don't hesitate to contact us. We value your input and are dedicated to providing the best possible service to our users.</p>
-        </li>
+        
       </ul>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,52 +1,52 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Alert } from "react-bootstrap";
 import "./Login.scss";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const [error, setError] = useState("");
+  
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
-
-  console.log(isLoggedIn, "isLoggedIn");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    username === "admin" && password === "admin"
-      ? setIsLoggedIn(true) && console.log("Logged in")
-      : console.log("Wrong username or password");
+    if (username === "admin" && password === "admin") {
+      setIsLoggedIn(true);
+      console.log("Logged in");
+    } else {
+      setError("Wrong username or password");
+    }
   };
 
   return (
-    <div className="login-container bg-success-subtle">
+    <div className="login-container">
       <h1>RazBet</h1>
 
       <Form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
-        <Button type="submit" value="Login">
+        {error && <Alert variant="danger">{error}</Alert>}
+
+        <Button variant="primary" type="submit">
           Login
         </Button>
       </Form>
